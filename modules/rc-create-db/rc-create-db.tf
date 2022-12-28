d99b194e0740d1e779aa62966126a5e0346d6f2d
brandonamos <bpamos21@gmail.com>
2022-12-28 13:24:26 -0700

    added create db module

---
+++ modules/rc-create-db/rc-create-db.tf
@@ -0,0 +1,23 @@
+
+
+// The primary database to provision
+resource "rediscloud_subscription_database" "example" {
+    subscription_id                       = var.rediscloud_subscription_id
+    name                                  = var.rc_db_name
+    protocol                              = "redis"
+    memory_limit_in_gb                    = var.rc_db_memory_limit_in_gb
+    data_persistence                      = var.rc_db_data_persistence
+    throughput_measurement_by             = var.rc_db_throughput_measurement_by
+    throughput_measurement_value          = var.rc_db_throughput_measurement_value
+    support_oss_cluster_api               = var.rc_db_support_oss_cluster_api
+    external_endpoint_for_oss_cluster_api = var.rc_db_external_endpoint_for_oss_cluster_api
+    replication                           = var.rc_db_replication
+    average_item_size_in_bytes            = var.rc_db_average_item_size_in_bytes
+    modules = var.rc_db_modules
+
+    # alert {
+    #   name = "dataset-size"
+    #   value = 40
+    # }
+
+}
