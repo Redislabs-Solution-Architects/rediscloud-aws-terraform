155d09b9f32e8d88afc3ee39dd3f9f6e48a253ca
brandonamos <bpamos21@gmail.com>
2023-01-11 14:33:01 -0700

    updated cidr notes and creation plan variables

--- modules/rc-subscription/rc-subscription.tf
+++ modules/rc-subscription/rc-subscription.tf
@@ -16,7 +16,7 @@ resource "rediscloud_subscription" "example" {
     cloud_account_id = var.rc_cloud_account_id
     region {
       region = var.rc_region
-      networking_deployment_cidr = var.rc_networking_deployment_cidr
+      networking_deployment_cidr = var.rc_networking_deployment_cidr #### MUST BE A /24 CIDR
       preferred_availability_zones = var.rc_preferred_availability_zones
       multiple_availability_zones  = true
     }
@@ -25,13 +25,13 @@ resource "rediscloud_subscription" "example" {
   // This block needs to be defined for provisioning a new subscription.
   // This allows creating a well-optimised hardware specification for databases in the cluster
   creation_plan {
-    average_item_size_in_bytes = var.average_item_size_in_bytes
-    memory_limit_in_gb = var.memory_limit_in_gb
-    quantity = var.quantity
-    replication= var.replication
-    support_oss_cluster_api= var.support_oss_cluster_api
-    throughput_measurement_by = var.throughput_measurement_by
-    throughput_measurement_value = var.throughput_measurement_value
-    modules = var.rc_modules
+    average_item_size_in_bytes = var.rc_cp_average_item_size_in_bytes
+    memory_limit_in_gb = var.rc_cp_memory_limit_in_gb
+    quantity = var.rc_cp_quantity
+    replication= var.rc_cp_replication
+    support_oss_cluster_api= var.rc_cp_support_oss_cluster_api
+    throughput_measurement_by = var.rc_cp_throughput_measurement_by
+    throughput_measurement_value = var.rc_cp_throughput_measurement_value
+    modules = var.rc_cp_modules
   }
 }
