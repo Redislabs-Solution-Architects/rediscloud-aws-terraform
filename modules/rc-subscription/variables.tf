f401418167f56b7f765289a3466e03e027c88e69
brandonamos <bpamos21@gmail.com>
2023-01-11 14:32:19 -0700

    added creation plan variables

--- modules/rc-subscription/variables.tf
+++ modules/rc-subscription/variables.tf
@@ -42,44 +42,45 @@ variable "rc_memory_storage" {
     default = "ram"
 }
 
-##### Redis Cloud Database Variables
-variable "average_item_size_in_bytes" {
-    description = ""
+##### Redis Cloud Creation Plan Variables
+##### The Creation plan defines the infra for the RE Subscription Cluster, it does not deploy a db.
+variable "rc_cp_average_item_size_in_bytes" {
+    description = "Relevant only to ram-and-flash clusters. Estimated average size (measured in bytes) of the items stored in the database. The value needs to be the maximum average item size defined in one of your databases."
     default = 1
 }
 
-variable "memory_limit_in_gb" {
-    description = ""
+variable "rc_cp_memory_limit_in_gb" {
+    description = "Maximum memory usage that will be used for your largest planned database."
     default = 25
 }
 
-variable "quantity" {
-    description = ""
+variable "rc_cp_quantity" {
+    description = "The planned number of databases in the subscription."
     default = 1
 }
 
-variable "replication" {
-    description = ""
+variable "rc_cp_replication" {
+    description = "Databases replication. Set to true if any of your databases will use replication."
     default = true
 }
 
-variable "support_oss_cluster_api" {
-    description = ""
+variable "rc_cp_support_oss_cluster_api" {
+    description = "Support Redis open-source (OSS) Cluster API."
     default = false
 }
 
-variable "throughput_measurement_by" {
-    description = ""
+variable "rc_cp_throughput_measurement_by" {
+    description = "Throughput measurement method that will be used by your databases, (either 'number-of-shards' or 'operations-per-second')"
     default = "operations-per-second"
 }
 
-variable "throughput_measurement_value" {
-    description = ""
+variable "rc_cp_throughput_measurement_value" {
+    description = "Throughput value that will be used by your databases (as applies to selected measurement method). The value needs to be the maximum throughput measurement value defined in one of your databases."
     default = 12000
 }
 
-variable "rc_modules" {
-    description = ""
+variable "rc_cp_modules" {
+    description = "a list of modules that will be used by the databases in this subscription. Not currently compatible with 'ram-and-flash' memory storage."
     type        = list(any)
     default = ["RedisJSON"]
 }
